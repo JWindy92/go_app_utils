4f23dcb5605a2cb3d934e41f7145a065898effde
John Widmaier <johnwid92@gmail.com>
2025-09-02 13:38:09 -0400

    fix parameter type

--- pkg/database/common.go
+++ pkg/database/common.go
@@ -8,6 +8,6 @@ type GormDB struct {
 	DB *gorm.DB
 }
 
-func (db *GormDB) RunAutoMigrate(models any) error {
+func (db *GormDB) RunAutoMigrate(models ...interface{}) error {
 	return db.DB.AutoMigrate(models)
 }
